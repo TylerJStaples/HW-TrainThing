b98ef514d307bf78a8fd6f669aeb4eb6922ee1a2
Tyler Staples <TylerJStaples@gmail.com>
2018-04-24 18:29:27 -0400

    Framework

---
+++ train.js
@@ -0,0 +1,11 @@
+var config = {
+    apiKey: "AIzaSyDhtGbExTIC7esDY9UdfwAUuj9203XaKWA",
+    authDomain: "train-homework-f46cb.firebaseapp.com",
+    databaseURL: "https://train-homework-f46cb.firebaseio.com",
+    projectId: "train-homework-f46cb",
+    storageBucket: "train-homework-f46cb.appspot.com",
+    messagingSenderId: "813578751531"
+  };
+firebase.initializeApp(config);
+
+var trainList = [];
